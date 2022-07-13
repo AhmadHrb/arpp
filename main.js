f74808e9d162967474538ca8fc1170a771a36894
ahmad <ahmadharb2115@gmail.com>
2022-07-13 10:23:03 +0300

    Added createRoom, forward and right. save and init are now functions

--- main.js
+++ main.js
@@ -4,14 +4,27 @@
  * Copyright AhmadHrb. All Rights Reserved.
  */
 
+const Drawing = require('dxf-writer');
+let d = new Drawing();
+const fs = require('fs');
+
 let renderType = 0; //2d or 3d
 
 // current position, used to draw
 let currentX = 0;
 let currentY = 0;
 
+exports.init = function() {
+    d.setUnits('Centimeters');
+    d.addLayer('main', Drawing.ACI.RED, 'CONTINUOUS');
+    d.setActiveLayer('main');
+
+}
+exports.save = function () {
+    fs.writeFileSync('building.dxf', d.toDxfString());
+}
 // change 2d and 3d rendering
-function setRender(type) {
+exports.setRender = function (type) {
     if (type == "2d") {
         renderType = 0;
     } else if (type == "3d") {
@@ -21,3 +34,14 @@ function setRender(type) {
     }
 }
 
+exports.createRoom = function (width,height) {
+if (renderType == 0) {
+    d.drawRect(currentX,currentY,width,height);
+}
+}
+exports.forward = function (steps) {
+    currentY += steps;
+}
+exports.right = function (steps) {
+    currentX += steps;
+}
